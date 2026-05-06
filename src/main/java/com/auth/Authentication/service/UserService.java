8eaff0f4b18afa4430abea5b59aed6899157373a
Nitish <nitishchaudhary245@gmail.com>
2026-05-06 15:05:30 +0530

    Removed IDE and build files

--- src/main/java/com/auth/Authentication/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.auth.Authentication.service;
-
-import com.auth.Authentication.model.User;
-
-public interface UserService {
-    User register(User user);
-    User login(String username, String password);
-}
